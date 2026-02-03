157cf06be9c3f2a8bfa09593e7cafda73bfb949b
Ayush29bit <ayushzztiwari69@gmail.com>
2026-02-03 22:00:17 +0530

    http request and response

---
+++ http/request_response.py
@@ -0,0 +1,24 @@
+
+from fastapi import APIRouter, Request, Response 
+
+router=APIRouter()
+
+@router.get("/inspect-request")
+async def inspect_request(request:Request):
+    return{
+        "method": request.method,
+        "url": str(request.url),
+        "query_params": dict(request.query_params),
+        "headers": dict(request.headers),
+        "client": request.client.host,
+    }
+
+@router.post("/custom-response")
+async def custom_response():
+    response=Response(
+        content="Created resource",
+        media_type="text/plain",
+        status_code=201
+    )
+
+    return response
