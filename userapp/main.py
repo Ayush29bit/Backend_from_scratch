e521fb4b0a8f427803948dd1ba0c4790612b77e4
Ayush Tiwari <ayushzztiwari69@gmail.com>
2026-02-18 12:00:11 +0530

    get methods with query and path params

--- userapp/main.py
+++ userapp/main.py
@@ -2,7 +2,25 @@ from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel, EmailStr, Field 
 from typing import Optional 
 from datetime import datetime
+from user_db import users_db
+from schema import UserCreate, UserReplace, UserUpdate, UserResponse
 
 app = FastAPI()
 
+@app.get("/users",
+         response_model=list[UserResponse],
+         status_code=status.HTTP_200_OK)
+def get_all_users(skip : int = 0, limit : int = 10, role : Optional[str] = None):
+    
+    users = list(users_db.values())
+    if role:
+        users = [user for user in users if user["role"] == role]
+    return users[skip: skip + limit]
 
+@app.get("users/{user_id}",
+         response_model=UserResponse,
+         status_code=status.HTTP_200_OK)
+def get_user(user_id:int)
+    if user_id not in users_db:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    return users_db[user_id]
